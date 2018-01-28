c148dc292129632508693857c8fe4583163c38c5
Jeff Will <jeffwill@sjp.com>
2018-01-28 01:04:41 -0500

    Initial commit of site architecture, content and basic styles

---
+++ app.js
@@ -0,0 +1,43 @@
+const htmlStandards = require('reshape-standard')
+const cssStandards = require('spike-css-standards')
+const jsStandards = require('spike-js-standards')
+const Records = require('spike-records')
+const pageId = require('spike-page-id')
+const sugarml = require('sugarml')
+const scss = require('postcss-scss')
+const lost = require('lost')
+const env = process.env.SPIKE_ENV
+const locals = {}
+
+module.exports = {
+  devtool: 'source-map',
+  matchers: { html: '*(**/)*.sgr', css: '*(**/)*.scss' },
+  ignore: ['**/layout.sgr', '**/_*', '**/.*', 'readme.md', 'yarn.lock', 'package-lock.json'],
+  vendor: 'assets/vendor/**',
+  module: {
+    rules: [{ test: /\.scss/, use: [{ loader: 'sass-loader' }] }]
+  },
+  plugins: [new Records({
+    addDataTo: locals,
+    work: { file: './assets/data/work.json' }
+  })],
+  reshape: htmlStandards({
+    parser: sugarml,
+    locals: (ctx) => {
+      locals.pageId = pageId(ctx)
+      return locals
+    },
+    minify: env === 'production'
+  }),
+  postcss: cssStandards({
+    parser: scss,
+    minify: env === 'production',
+    warnForDuplicates: env !== 'production',
+    appendPlugins: lost
+  }),
+  entry: { 'js/vendor': ['./assets/js/vendor.js'] },
+  babel: jsStandards(),
+  server: {
+    logLevel: 'info'
+  }
+}
---
+++ assets/js/vendor.js
@@ -0,0 +1,2 @@
+import '../vendor/fontawesome-free-5.0.4/js/fontawesome.js'
+import '../vendor/fontawesome-free-5.0.4/js/fa-brands.js'
