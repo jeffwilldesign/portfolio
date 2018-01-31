8d1b98f8e5889d36f288edd07b889e14e91cc04c
Jeff Will <jeffwill@sjp.com>
2018-01-30 23:25:39 -0500

    Added lory.js to add slider functionality and tidied up the typography and spacing for the intro card.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,15 @@
+import {lory} from 'lory.js'
+
 import '../vendor/fontawesome-free-5.0.4/js/fontawesome.js'
 import '../vendor/fontawesome-free-5.0.4/js/fa-brands.js'
+import '../vendor/fontawesome-free-5.0.4/js/fa-solid.js'
+
+document.addEventListener('DOMContentLoaded', () => {
+  const sliders = document.querySelectorAll('.js_slider')
+
+  for (let slider of sliders) {
+    lory(slider, {
+      infinite: 1
+    })
+  }
+})
